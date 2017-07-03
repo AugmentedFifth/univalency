4713aed493c70b1b762cbdffdc7abb69c70e7fe3
AugmentedFifth <zcomito@gmail.com>
2017-07-03 15:01:53 -0700

    turnerino

--- src/Univalency.hs
+++ src/Univalency.hs
@@ -34,6 +34,7 @@ data Action
     | StopMove Keyboard.Key
     | MouseMove (V2 Int)
     | MouseClick Mouse.MouseButton (V2 Int)
+    | MouseRelease Mouse.MouseButton (V2 Int)
 
 data PlayerStatus
     = Moving [Keyboard.Key]
@@ -45,8 +46,10 @@ data Model = Model
     { playerPos    :: V2 Double
     , playerVel    :: V2 Double
     , playerAcc    :: V2 Double
+    , playerAngle  :: Double
     , playerStatus :: PlayerStatus
     , mousePos     :: V2 Double
+    , mouseDown    :: Bool
     , clickEffects :: [ClickEffect]
     , debug        :: String
     }
@@ -87,16 +90,22 @@ squareForm :: Form e
 squareForm = filled squareColor (square 120)
 
 playerImpetus :: Double
-playerImpetus = 0.03
+playerImpetus = 3e-2
 
 controlAcc :: Double
-controlAcc = 0.0075
+controlAcc = 7.5e-3
 
 frictionalAcc :: Double
 frictionalAcc = 1e-4
 
 playerMaxSqSpeed :: Double
 playerMaxSqSpeed = 0.375
+
+minTurnVel :: Double
+minTurnVel = 0.015625
+
+turnVel :: Double
+turnVel = 0.01
 
 epsilon :: Double
 epsilon = 1e-8
@@ -132,8 +141,10 @@ initial =
         { playerPos    = zero
         , playerVel    = zero
         , playerAcc    = zero
+        , playerAngle  = 0
         , playerStatus = Waiting
         , mousePos     = zero
+        , mouseDown    = False
         , clickEffects = []
         , debug        = ""
         }
@@ -146,8 +157,9 @@ update model@Model{..} (Animate dt) =
         { playerPos    = newPos
         , playerVel    = newVel
         , playerAcc    = newAcc
+        , playerAngle  = newAngle
         , clickEffects = clickEffects >>= decrementClickEffects
-        , debug        = "pos: "
+        , debug        =  "pos: "
                       ++ showV2 4 (round <$> newPos :: V2 Int)
                       ++ "  ||  vel: "
                       ++ showV2 5 ((round.(10000 *)) <$> newVel :: V2 Int)
@@ -165,18 +177,39 @@ update model@Model{..} (Animate dt) =
             where
                 newSpeed = acc * dt + speed
 
+        updateVel maxSqSpeed vel acc
+            | newSqSpeed <= epsilon   = zero
+            | newSqSpeed > maxSqSpeed = sqrt maxSqSpeed *^ signorm newVel'
+            | otherwise               = newVel'
+            where
+                newVel' = liftI2 updateSpeed vel acc
+                newSqSpeed = quadrance newVel'
+
         newPos = playerVel ^* dt + playerPos
-        newVel' = liftI2 updateSpeed playerVel playerAcc
-        newSqSpeed = quadrance newVel'
-        newVel
-            | newSqSpeed <= epsilon         = zero
-            | newSqSpeed > playerMaxSqSpeed = sqrt playerMaxSqSpeed *^
-                                                  signorm newVel'
-            | otherwise                     = newVel'
+
+        newVel = updateVel playerMaxSqSpeed playerVel playerAcc
+
         newAcc = ((frictionalAcc * dt *^).negate.normalize) newVel +
             case playerStatus of
                 Moving keys -> controlAcc *^ (signorm.sum) (direction <$> keys)
                 _           -> zero
+
+        newAngle = playerAngle +
+            if not mouseDown then 0 else deltaAngle
+            where
+                towardCursor@(V2 _ ty) = mousePos - (newPos + windowCenter)
+                V2 x _ = normalize towardCursor
+                destAngle = acos x * signum ty
+                angleDiff' = destAngle - playerAngle
+                angleDiff = angleDiff' -
+                    if abs angleDiff' > pi then
+                        signum angleDiff' * 2 * pi
+                    else
+                        0
+                deltaAngle = signum angleDiff
+                           * min
+                               (minTurnVel + abs angleDiff * turnVel * dt)
+                               (abs angleDiff / 2)
 
         decrementClickEffects (Active i pos)
             | i > 0     = [Active (i - 1) pos]
@@ -191,7 +224,7 @@ update model@Model{..} (StartMove key) =
         if updating then
             ( model
                 { playerVel    = playerVel + playerImpetus *^ direction key
-                , playerAcc    = playerAcc + controlAcc *^ direction key
+                , playerAcc    = newAcc
                 , playerStatus = newStatus
                 }
             , Cmd.none
@@ -199,6 +232,8 @@ update model@Model{..} (StartMove key) =
         else
             (model, Cmd.none)
     where
+        newAcc = playerAcc + controlAcc *^ direction key
+
         newStatus = case playerStatus of
             Moving keys -> Moving $ key : keys
             _           -> Moving [key]
@@ -218,13 +253,24 @@ update model@Model{..} (MouseMove mousePos') =
 
 update model@Model{..} (MouseClick button clickPos) =
     if button == Mouse.LeftButton then
-        (model { clickEffects = newClickEffects }, Cmd.none)
+        ( model
+            { mouseDown    = True
+            , clickEffects = newClickEffects
+            }
+        , Cmd.none
+        )
     else
         (model, Cmd.none)
     where
         samePos (Active _ pos) = pos /= clickPos
         filteredClickEffects = filter samePos clickEffects
         newClickEffects = Active 24 clickPos : filteredClickEffects
+
+update model@Model{..} (MouseRelease button _) =
+    if button == Mouse.LeftButton then
+        (model { mouseDown = False }, Cmd.none)
+    else
+        (model, Cmd.none)
 
 update model DoNothing = (model, Cmd.none)
 
@@ -237,13 +283,14 @@ subscriptions = Sub.batch
           \key -> if key `elem` movementKeys then StopMove key else DoNothing
     , Mouse.moves MouseMove
     , Mouse.downs MouseClick
+    , Mouse.ups MouseRelease
     ]
 
 view :: Model -> Graphics SDLEngine
 view Model{..} = Graphics2D
     $ collage
         [ centerForms (V2 xCenter 20) [text $ toText debug]
-        , centerForms windowCenter [setPos playerPos squareForm]
+        , centerForms windowCenter [rotate playerAngle.setPos playerPos $ squareForm]
         , group $ clickEffectForm <$> clickEffects
         ]
     where
